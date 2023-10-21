1a0b759c77c86eb4d7194c5cc5dc056d62049c93
Bohdan Chornoivan <bohdan.chornoivan@gmail.com>
2023-10-21 15:23:16 +0300

    add vecrot DeckRealease

--- desk.h
+++ desk.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "hand.h"
 #include "generic_player.h"
+#include <vector>
+#include "card.h"
 
 class Desk : public Hand
 {
@@ -11,5 +13,7 @@ public:
 	void Shuffle();
 	void Deal(Hand& aHand);
 	void AdditionalCards(GenericPlayer& aGenericPlayer);
+protected:
+	std::vector<Card*> DeckRelease;
 };
 
